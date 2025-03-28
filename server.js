00ab2fb6617ae98944005313f35538c1dc7d726e
RayanGhannam <rayan.mahmoudgh@gmail.com>
2025-03-28 04:05:14 +0300

    Initial commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
+
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -9,30 +10,29 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({ 
-  // connect to your own database here:
+// Database Connection
+const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+  connection: process.env.DATABASE_URL || {
+  },
 });
 
 const app = express();
 
+app.use(cors());
+app.use(express.json()); // Express has built-in JSON parsing, no need for body-parser
 
-app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
+app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt));
+app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
+app.put('/image', (req, res) => image.handleImage(req, res, db));
+app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
