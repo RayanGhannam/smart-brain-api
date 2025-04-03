6356eb3b52f116d6515de0595fc5ec3287689a71
RayanGhannam <rayan.mahmoudgh@gmail.com>
2025-04-03 12:22:51 +0300

    Fix Vercel deployment

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Express has built-in JSON parsing, no need for body-parser
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
-
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
 app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt));
 app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
